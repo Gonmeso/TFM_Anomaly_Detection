3942d68ce3fd93a6aacad97323b80285f003f395
Gonmeso <gmellizosoto@gmail.com>
2019-04-30 17:54:22 +0000

    Add generate_graphs and the first hourly grpahs generate (not final)

---
+++ src/generate_graphs.py
@@ -0,0 +1,73 @@
+import os
+import logging
+import pandas as pd
+import matplotlib.pyplot as plt
+
+PRE_PATH = '../data/preprocessed/'
+LOGS_PATH = '../logs/'
+PLOTS_PATH= './plots/'
+
+logging.basicConfig(format='%(levelname)s - %(asctime)s: %(message)s',
+                    datefmt='%d/%m/%Y %H:%M:%S',
+                    filename=(LOGS_PATH + 'generate_graphs.log'),
+                    filemode='w',
+                    level=logging.INFO)
+
+if not os.path.exists(PLOTS_PATH):
+    logging.warning("Plots directory does not exist")
+    logging.info("Creating plots dir")
+    os.mkdir(PLOTS_PATH)
+
+logging.info('Starting graphs.')
+
+files_list = [x for x in os.listdir(PRE_PATH) if 'cmd' in x]
+
+for file_ in files_list:
+    try:
+        logging.info('-'*20)
+
+        df = pd.read_csv((PRE_PATH + file_), sep=';', usecols=[0])
+        df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+        df = df['Timestamp'].value_counts().sort_index()
+
+        logging.info(f"File {file_} loaded")
+
+        year = df.index.year.unique().values[0]
+        str_template = "{year}-{month}-{day} {hour}:{minute}:{second}"
+
+        for month in df.index.month.unique().values:
+
+            for day in df.index.day.unique():
+
+                fig, ax = plt.subplots(nrows=6, ncols=4, figsize=(12, 8))
+
+                for hour in range(24):
+                    init = str_template.format(
+                        year=year,
+                        month=month,
+                        day=day,
+                        hour=hour,
+                        minute='00',
+                        second='00'
+                    )
+
+                    end = str_template.format(
+                        year=year,
+                        month=month,
+                        day=day,
+                        hour=hour,
+                        minute='59',
+                        second='59'
+                    )
+                    if not df[init:end].empty:
+                        logging.info(f"Plotting graph for interval {hour}-{hour + 1} @ day {day}")
+                        df[init:end].plot(ax=ax[hour%6][hour//6], x='index', y='Timestamp')
+                    else:
+                        logging.warning(f"No data for interval {hour}-{hour + 1} @ day {day}")
+
+                plt.savefig(PLOTS_PATH + file_ + '_' + str(month) + '_hourly_plots.png')
+                logging.info(f"Graph for {file_} created")
+    except Exception as e:
+        logging.error(str(e))
+
+logging.info("Process finished")
