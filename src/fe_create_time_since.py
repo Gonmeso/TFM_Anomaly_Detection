5eba4dc904e8d0e824cf7fa3a3f1a5df02d75c78
Gonmeso <gmellizosoto@gmail.com>
2019-05-21 17:12:19 +0000

    Adds feature for IP1-IP2 time since
    ~fe_create_time_since in order to generate a newer feature regarding time since last conection between
    ip tuples

--- src/fe_create_time_since.py
+++ src/fe_create_time_since.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import logging
 import os
+from time import time
 from helper.helpers import *
 from multiprocessing import Pool
 from functools import partial
@@ -15,10 +16,10 @@ def to_seconds(x):
     return x.total_seconds()
 
 
-def read_tsv(filepath):
+def read_tsv(filepath, **kwargs):
     data = pd.read_csv(filepath,
                        sep='\t',
-                       parse_dates=['Timestamp'])
+                       **kwargs)
     return data
 
 
@@ -41,12 +42,37 @@ def create_time_diff(data, var_list):
     return data
 
 
+def create_diff_ip1_ip2(data, var_list):
+
+    subset = data[var_list].drop_duplicates()
+    col_ip_1 = subset[var_list[0]].values
+    col_ip_2 = subset[var_list[1]].values
+    time_name = 'IP1_IP2_time_diff_per_value'
+    data[time_name] = 0
+    data[time_name] = data[time_name].astype('float64')
+
+    logging.info(f'Starting creation of {time_name}')
+    logging.info(f'Total unique values: {len(col_ip_1)}')
+    for ip_1, ip_2 in zip(col_ip_1, col_ip_2):
+        start = time()
+        idx = (data['IP1'] == ip_1) & (data['IP2'] == ip_2)
+        tmp = pd.DataFrame(data[idx]['Timestamp'])
+        tmp = tmp.diff()
+        data[time_name][idx] = tmp.iloc[:, 0].apply(to_seconds).values
+        end = time()
+        logging.debug(f'Time elapsed: {end - start}s')
+    logging.info(f'{time_name} created!')
+
+    return data
+
+
 def execute(filename, path, var_list):
 
     filepath = os.path.join(path, filename)
-    data = read_tsv(filepath)
+    data = read_tsv(filepath, parse_dates=['Timestamp'])
     logging.info(f'{filename} loaded!')
     data = create_time_diff(data, var_list)
+    data = create_diff_ip1_ip2(data, var_list)
     save_to_tsv(data, path, filename)
     logging.info(f'{filename} saved!')
 
@@ -59,7 +85,6 @@ def main():
 
     var_list = ['IP1', 'IP2']
     files_list = get_file_list(DATA_PATH, 'csv')
-    # execute(files_list[0], DATA_PATH, var_list)
     pool = Pool(processes=N_PROCESSES)
     execute_pool = partial(execute, path=DATA_PATH, var_list=var_list)
 
